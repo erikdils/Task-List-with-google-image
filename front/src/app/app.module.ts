9a7af1ce03d30ebe9f19ec4fc91d4d4dda5143c5
erikdils <erikdils@gmail.com>
2020-05-02 14:18:33 +0200

    add title & description

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -23,7 +24,7 @@ import { FooterComponent } from './parts/footer/footer.component';
     HeaderComponent,
     FooterComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- front/src/app/pages/main/main.component.ts
+++ front/src/app/pages/main/main.component.ts
@@ -1,6 +1,10 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 import { Component, OnInit } from '@angular/core';
+
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+};
 
 @Component({
   selector: 'app-main',
@@ -10,12 +14,17 @@ import { Component, OnInit } from '@angular/core';
 
 export class MainComponent implements OnInit {
 
-  taskList: any;      // Array<any>;
+  taskList: any = [];      // Array<any>;
+  titleInput: string;
+  descriptionInput: string;
   constructor(private http: HttpClient) { }
 
   async ngOnInit() {
+    this.refreshTaskList()
+  }
+
+  async refreshTaskList() {
     this.taskList = await this.getTasks()
-    console.log(this.taskList);
   }
 
   getTasks() {
@@ -26,6 +35,36 @@ export class MainComponent implements OnInit {
     console.log(i)
     this.taskList[i][0].edit = true;
   }
+
+  total() {
+    let total = 0
+    this.taskList.forEach((el) => {
+      if (el[0].price) total += el[0].price
+    })
+    return total
+  }
+
+amounthInprogres() {
+  return this.taskList.filter((t) => (!t[0].done) ? true : false).length
+  };
+
+ amounthDone() {
+  return this.taskList.filter((t) => (!t[0].done) ? false : true).length
+  };
+
+  async add() {
+    const task = {
+      title: this.titleInput,
+      description: this.descriptionInput
+    }
+    await this.sendTask(task);
+    this.refreshTaskList();
+  }
+
+  sendTask(task) {
+    return this.http.post('http://localhost:3007/task', task).toPromise();
+  }
+
 
 
 }
